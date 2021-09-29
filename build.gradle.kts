f77142bb109697fe049a8a856e597e8a5f154f47
Gan Xu <gan.xu@wustl.edu>
2021-09-29 00:15:42 -0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,16 @@ repositories {
     mavenCentral()
 }
 
+val ktor_version: String by project
+val logback_version: String by project
+
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.3.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-websockets:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 }
 
 tasks.test {
