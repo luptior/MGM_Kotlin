bef7b6c8d99e8b6a0de5ebb46a08e8efc51e819f
Gan Xu <gan.xu@wustl.edu>
2021-09-30 00:49:12 -0700

    update on build fiiles

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,31 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.30"
+    kotlin("multiplatform") version "1.3.10"
     kotlin("plugin.serialization") version "1.5.31"
     application
+}
+
+kotlin {
+    // Determine host preset.
+    val hostOs = System.getProperty("os.name")
+
+    // Create target for the host platform.
+    val hostTarget = when {
+        hostOs == "Mac OS X" -> macosX64("workers")
+        hostOs == "Mac OS X" -> macosArm64("workers")
+        hostOs == "Linux" -> linuxX64("workers")
+        hostOs.startsWith("Windows") -> mingwX64("workers")
+        else -> throw GradleException("Host OS '$hostOs' is not supported in Kotlin/Native $project.")
+    }
+
+    hostTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "sample.workers.main"
+            }
+        }
+    }
 }
 
 group = "me.gx"
@@ -14,7 +36,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.21")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.3.0")
 }
 
